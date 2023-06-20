c4e9beb91c772f74b558461bd2ebf558294996a4
Administrator <9202888607@mail.ru>
2023-06-20 22:13:56 +0300

    refactoring main

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from typing import List, Any
+
 from openpyxl import load_workbook, Workbook
 from pathlib import Path
 from user_interface import *
+
+
 
 def list_values(row_list): # Создание массива данных новой таблицы
     table_list_values.append(row_list)
     return table_list_values
 
-def filling_data(table_list_values,sheet_new, row = 0):# запись данных в новую таблицу
+def filling_data(table_list_values,sheet_new, row = 0): # запись данных в новую таблицу
     for item in table_list_values:
         row += 1
         sheet_new['A' + str(row)] = item[0]
@@ -14,29 +18,35 @@ def filling_data(table_list_values,sheet_new, row = 0):# запись данны
         sheet_new['C' + str(row)] = item[2]
 
 
-def new_workbook(file_name_new, table_list_values): # создание, заполнение и сохранение рабочей книги
+def new_workbook(file_name_new, table_list_values):  # создание, заполнение и сохранение рабочей книги
     wb = Workbook()  # новая рабочая книга
     sheet_new = wb.active
     sheet_new.title = file_name_new
     filling_data(table_list_values, sheet_new)  # запись данных в новую таблицу
     wb.save(file_name_new)
 
-file_name = ''
-dialog = A()
+#def row_columns(sheet):
+#    rows = sheet.max_row
+#    columns = sheet.max_column
+#    print(rows, columns)
+
+
+file_name: str = ''
+dialog = Interface()
 dialog.dialog_window()
 file_name = dialog.file_name
-print(file_name)
-file_name_new = Path(file_name).stem + '_new' + '.xlsx' # создал имя для нового файла
-table_list_values = [] # пустой массив данных для новой таблицы
-
+file_name_new = Path(file_name).stem + '_new' + '.xlsx'  # создал имя для нового файла
+table_list_values: list[Any] = []  # пустой массив данных для новой таблицы
 
 if len(file_name) > 0:
     wb2 = load_workbook(file_name, data_only=True)  # Загрузил файл в ячейках только значения
-    sheet = wb2['Лист1']
-    row_number = 0
-    for row in sheet.rows: # перебираю файл построчно
-        row_number += 1
-        list_values([row_number, ' '.join(map(str, [row[1].value, 'Размер', row[3].value, 'штрих код', row[8].value])), row[9].value]) # создал новую строку для записи в ячейку
-    new_workbook(file_name_new, table_list_values)
+    sheet: object = wb2['Лист1']
+#    row_columns(sheet)
 else:
     error_input_file()
+
+row_number = 0
+for row in sheet.rows:  # перебираю файл построчно
+    row_number += 1
+    list_values([row_number, ' '.join(map(str, [row[1].value, 'Размер', row[3].value, 'штрих код', row[8].value])), row[9].value])  # создал новую строку для записи в ячейку
+new_workbook(file_name_new, table_list_values)
--- user_interface.py
+++ user_interface.py
@@ -1,23 +1,29 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+#from tkinter import checkbutton
 
-class A:
 
+class Interface:
     def __init__(self):
         self.file_name = ""
-
+        self.checkvar1 = ''
 
     def open_file(self):
-        file_open = filedialog.askopenfilename(filetypes = (("Text files","*.xlsx"),("all files","*.*")))
+        file_open = filedialog.askopenfilename(filetypes=(("Text files", "*.xlsx"), ("all files", "*.*")))
         self.file_name = file_open
+
+    #def quit(self):
+    #    self.window.destroy()
 
     def dialog_window(self):
         window = Tk()
-        window.title("Сложение столбцов")
-        window.geometry('400x250')
-        btn = Button(window, text="Открыть файл", command=self.open_file)
-        btn.grid(column=1, row=1)
+        #window.title("Сложение столбцов")
+        #window.geometry('600x300')
+        btn = Button(text="Открыть файл", command=self.open_file)
+        btn.pack(side=RIGHT)
+        btn_close = Button(text="Сформировать файл", command=window.quit)
+        btn_close.pack(side=LEFT)
         window.mainloop()
 
 
